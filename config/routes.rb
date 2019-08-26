8801ba4a8558d75247d09a14e27dc3496ffdabd9
Genki Kurihara <spiral809@gmail.com>
2019-08-26 20:05:47 +0900

    changed icon to cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   resources :doses, only: [:destroy]
   root to: 'cocktails#index'
+  root to: 'pages#home'
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
